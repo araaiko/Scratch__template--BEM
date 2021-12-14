70bc5f1346fe3f52fedb8f4eca384ff89d5b6061
araaiko <araaiko.122@gmail.com>
2021-12-14 14:53:02 +0900

    不要パッケージ削除

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ const gulp = require("gulp");
 const sass = require("gulp-dart-sass");
 const plumber = require("gulp-plumber");
 const notify = require("gulp-notify");
-const sassGlob = require("gulp-sass-glob");
 const mmq = require("gulp-merge-media-queries");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
@@ -13,14 +12,10 @@ const cssdeclsort = require("css-declaration-sorter");
 /* browser-sync */
 const browserSync = require("browser-sync");
 
-/* select use compiler */
-sass.compiler = require("sass"); // Dart Sass = "sass"
-
 gulp.task("sass", function () {
     return gulp
         .src("./sass/**/*.scss")
         .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
-        .pipe(sassGlob())
         .pipe(sass({ outputStyle: "expanded" }))
         .pipe(postcss([autoprefixer()]))
         .pipe(postcss([cssdeclsort({ order: "alphabetical" })]))
